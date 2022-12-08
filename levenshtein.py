f07c450b7737aa690a6506c58481883641fc6bbc
tubukation <166604354+tubukation@users.noreply.github.com>
2022-12-08 11:21:37 +0000

    chore(package): update rollup to version 0.56.0

--- levenshtein.py
+++ levenshtein.py
@@ -18,4 +18,12 @@ def lev_subst(word):
     l = chr(ord('a') + random.randint(0,25))
     r = random.randint(1, len(word) -1)
     w = word[:r] + l + word[r+1:]  
-    assert(l
+    assert(len(w) == len(word))
+    return w
+
+def lev2(word):
+    r = random.randint(0,2)
+    if r == 0: 
+        return lev_del(lev_del(word))
+    elif r == 1: 
+        return lev_add(lev_add
