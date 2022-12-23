7b5dea6fa8082b1e327f55928f11dfff1f8d29e4
tubukation <166604354+tubukation@users.noreply.github.com>
2022-12-23 14:35:32 +0000

    Added entities

--- pa1-spamlord-v2/python/SpamLord0.py
+++ pa1-spamlord-v2/python/SpamLord0.py
@@ -254,3 +254,176 @@ def get_emails(line):
                 #print 'before:', matches
                 matches = [tuple([re.sub('\s+', '.', x) for x in m]) for m in matches]
                 #print 'after: ', matches
+    if len(matches):
+        for m in matches:
+            # Filter multiple matches in regex.
+            #m = tuple([x for x in m if x])
+            
+            if len(m) != 3:
+                print '-' * 80
+            print m
+            
+            # Replace space with . as capturing regex's allow spaces
+            m = tuple([re.sub('\sdom\s|\sdo?t\s|\s', '.', x) for x in m])
+            print m
+            #if any(['engler' in x for x in m]):
+            #    exit()
+                
+            m = m[1:]  # Remove email    
+            
+            if m[0].lower() in name_exclusions:
+                continue
+            email = '%s@%s.%s' % (m)
+            emails_found.append(email)
+    
+    return emails_found
+
+regex_phone_approx = re.compile(r'''
+    (\d{3})\D{0,3}(\d{3})\D{0,3}(\d{4})
+    ''', 
+    re.VERBOSE)  
+    
+regex_phone = re.compile(r'''
+    (\d{3})[\s\-\)]{1,3}(\d{3})[\s\-]{1,3}(\d{4})
+    ''', 
+    re.VERBOSE)
+    
+if False:
+    line = '6507234377 650-723-4377'
+    matches = regex_phone_approx.findall(line)
+    print line
+    print matches
+    exit()
+    
+def get_phones(line):
+    phones_found = []
+    matches = regex_phone.findall(line)
+    for m in matches:
+        print '-' * 80
+        print m
+        phones_found.append('%s-%s-%s' % m)
+    return phones_found    
+
+""" 
+TODO
+This function takes in a filename along with the file object (or
+an iterable of strings) and scans its contents against regex patterns.
+It returns a list of (filename, type, value) tuples where type is either
+and 'e' or a 'p' for e-mail or phone, and value is the formatted phone
+number or e-mail.  The canonical formats are:
+     (name, 'p', '###-###-#####')
+     (name, 'e', 'someone@something')
+If the numbers you submit are formatted differently they will not
+match the gold answers
+
+NOTE: ***don't change this interface***, as it will be called directly by
+the submit script
+"""
+def process_file(name, f):
+    # note that debug info should be printed to stderr
+    # sys.stderr.write('[process_file]\tprocessing file: %s\n' % (path))
+    if True:
+        res = []
+        for line in f:
+            emails_found = get_emails(line)
+            for email in emails_found:
+                res.append((name,'e',email))
+            phones_found = get_phones(line)
+            for phone in phones_found:
+                res.append((name,'p',phone))    
+        return res
+    else:
+        emails = [(name,'e',email) for line in f for email in get_emails(line)]
+        #emails = []
+        phones = [(name,'p',phone) for line in f for phone in get_phones(line)]
+        return emails + phones
+
+if False:
+    def test_file(fname):
+        f = open(fname, 'r')
+        f_guesses = process_file(fname, f)
+        print fname, f_guesses
+    
+    test_file(r'..\data\dev\pal')   
+    exit()
+
+"""
+You should not need to edit this function, nor should you alter
+its interface as it will be called directly by the submit script
+"""
+def process_dir(data_path):
+    # get candidates
+    guess_list = []
+    for fname in os.listdir(data_path):
+        if fname[0] == '.':
+          continue
+        path = os.path.join(data_path,fname)
+        f = open(path,'r')
+        f_guesses = process_file(fname, f)
+        guess_list.extend(f_guesses)
+    return guess_list
+
+"""
+You should not need to edit this function.
+Given a path to a tsv file of gold e-mails and phone numbers
+this function returns a list of tuples of the canonical form:
+(filename, type, value)
+"""
+def get_gold(gold_path):
+    # get gold answers
+    gold_list = []
+    f_gold = open(gold_path,'r')
+    for line in f_gold:
+        gold_list.append(tuple(line.strip().split('\t')))
+    return gold_list
+
+"""
+You should not need to edit this function.
+Given a list of guessed contacts and gold contacts, this function
+computes the intersection and set differences, to compute the true
+positives, false positives and false negatives.  Importantly, it
+converts all of the values to lower case before comparing
+"""
+def score(guess_list, gold_list):
+    guess_list = [(fname, _type, value.lower()) for (fname, _type, value) in guess_list]
+    gold_list = [(fname, _type, value.lower()) for (fname, _type, value) in gold_list]
+    guess_set = set(guess_list)
+    gold_set = set(gold_list)
+
+    tp = guess_set.intersection(gold_set)
+    fp = guess_set - gold_set
+    fn = gold_set - guess_set
+
+    pp = pprint.PrettyPrinter()
+    #print 'Guesses (%d): ' % len(guess_set)
+    #pp.pprint(guess_set)
+    #print 'Gold (%d): ' % len(gold_set)
+    #pp.pprint(gold_set)
+    print 'True Positives (%d): ' % len(tp)
+    pp.pprint(tp)
+    print 'False Positives (%d): ' % len(fp)
+    pp.pprint(fp)
+    print 'False Negatives (%d): ' % len(fn)
+    pp.pprint(fn)
+    print 'Summary: tp=%d, fp=%d, fn=%d' % (len(tp),len(fp),len(fn))
+
+"""
+You should not need to edit this function.
+It takes in the string path to the data directory and the
+gold file
+"""
+def main(data_path, gold_path):
+    guess_list = process_dir(data_path)
+    gold_list = get_gold(gold_path)
+    score(guess_list, gold_list)
+
+"""
+commandline interface takes a directory name and gold file.
+It then processes each file within that directory and extracts any
+matching e-mails or phone numbers and compares them to the gold file
+"""
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'usage:\tSpamLord.py <data_dir> <gold_file>'
+        sys.exit(0)
+    main(sys.argv[1],sys.argv[2])
---
+++ pa1-spamlord-v2/python/crawler.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python
+
+"""
+Original author: Paul Boddie <paul@boddie.org.uk>
+
+To the extent possible under law, the person who associated CC0 with this work
+has waived all copyright and related or neighboring rights to this work.
+
+See: http://creativecommons.org/publicdomain/zero/1.0/
