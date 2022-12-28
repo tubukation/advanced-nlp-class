886b3efe4a5b0910c7659d3bb93a9c1bbe92a82f
tubukation <166604354+tubukation@users.noreply.github.com>
2022-12-28 06:19:52 +0000

    8.5.0

--- pa1-spamlord-v2/python/test.py
+++ pa1-spamlord-v2/python/test.py
@@ -1,4 +1,8 @@
 import sys
 import SpamLord
 
-d
+def get_personal_info(path):
+    data = file(path, 'rt').read()
+    result = SpamLord.extract_personal_info(path, data)
+    print result
+    uniques = dict([(e[2].strip('\n'), (e[0]
