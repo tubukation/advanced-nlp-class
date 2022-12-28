0f608156fd4a62efba15e800f63fc1376da4843e
tubukation <166604354+tubukation@users.noreply.github.com>
2022-12-28 06:58:34 +0000

    Incorporated from BSD 4.4-Lite.

--- pa1-spamlord-v2/python/test.py
+++ pa1-spamlord-v2/python/test.py
@@ -5,4 +5,10 @@ def get_personal_info(path):
     data = file(path, 'rt').read()
     result = SpamLord.extract_personal_info(path, data)
     print result
-    uniques = dict([(e[2].strip('\n'), (e[0]
+    uniques = dict([(e[2].strip('\n'), (e[0],e[1])) for e in result])
+    print '\n'.join(['%30s : %s' % (k,v) for (k,v) in uniques.items()])
+
+    
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        D('Usage: 
