eb520a115b460056b6217c7b0650a7f5152264c2
tubukation <166604354+tubukation@users.noreply.github.com>
2023-01-18 14:04:54 +0000

    sparc64: ldc_connect() should not return EINVAL when handshake is in progress.

--- pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
+++ pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
@@ -31,4 +31,9 @@ _NEGATIVES = set([
     ])    
 _ENGLISH_STOP_WORDS = set([ 
     'a', 'a\'s', 'able', 'about', 'above', 'according', 'accordingly', 'across', 
-    'actually', 'afte
+    'actually', 'after', 'afterwards', 'again', 
+    'against', # Seems to be a key word to omit
+    'ain\'t', 'all', 
+    'allow', 'allows', 'almost', 'alone', 'along', 'already', 'also', 
+    'although', 
+    'always', 'am', '
