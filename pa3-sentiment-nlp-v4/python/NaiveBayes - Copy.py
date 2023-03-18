ed7f436324dabd49a5d5c366fc8d1b4ae11a0099
tubukation <166604354+tubukation@users.noreply.github.com>
2023-03-18 22:06:05 +0000

    Updated 'css/creative.css' via CloudCannon

--- pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
+++ pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
@@ -83,4 +83,8 @@ _ENGLISH_STOP_WORDS = set([
     'it', 'it\'d', 'it\'ll', 'it\'s', 
     'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kept', 'know', 'known', 
     'knows', 'l', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 
-    'lest',
+    'lest', 'let', 'let\'s', 'like', 
+    #'liked',
+    'likely', 'little', 'look', 
+    'looking', 'looks', 'ltd', 'm', 'mainly', 'many', 'may', 'maybe', 'me', 'mean', 
+    'meanw
