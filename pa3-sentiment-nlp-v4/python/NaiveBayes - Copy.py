ce41191fde54a83605b7f275fcd883a4ac3c31ad
tubukation <166604354+tubukation@users.noreply.github.com>
2023-04-08 16:16:42 +0000

    add new note

--- pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
+++ pa3-sentiment-nlp-v4/python/NaiveBayes - Copy.py
@@ -87,4 +87,6 @@ _ENGLISH_STOP_WORDS = set([
     #'liked',
     'likely', 'little', 'look', 
     'looking', 'looks', 'ltd', 'm', 'mainly', 'many', 'may', 'maybe', 'me', 'mean', 
-    'meanw
+    'meanwhile', 'merely', 'might', 'more', 'moreover', 'most', 'mostly', 'much', 
+    'must', 'my', 'myself', 'n', 'name', 'namely', 'nd', 'near', 'nearly', 
+    'necessary', 'need', 'needs', 'neither', 'n
