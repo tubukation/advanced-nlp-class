93c2aa6d7188d45070c12fb28f8ac7185dddb01d
tubukation <166604354+tubukation@users.noreply.github.com>
2023-09-08 13:42:43 +0000

    UT fixes

--- pa3-sentiment-nlp-v4/python/NaiveBayes_peter.py
+++ pa3-sentiment-nlp-v4/python/NaiveBayes_peter.py
@@ -4,4 +4,7 @@
 
 #
 # The area for you to implement is marked with TODO!
-# Generally, you should not need to touch things
+# Generally, you should not need to touch things *not* marked TODO
+#
+# Remember that when you submit your code, it is not run from the command line 
+# and your main() will *not* be run. To be safest, restrict your ch
