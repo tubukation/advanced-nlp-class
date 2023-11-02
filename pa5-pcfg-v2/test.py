535f9d31bce64bef7865cb57177b1c2ce10eacfb
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-02 20:21:18 +0000

    Мелкие правки

--- pa5-pcfg-v2/test.py
+++ pa5-pcfg-v2/test.py
@@ -1,4 +1,18 @@
 word_text = '''1	JJ	bloody
 1	JJ	weary
 1	JJ	unable
-1	JJ	tru
+1	JJ	trusty
+1	JJ	further
+1	JJ	sacred
+1	JJ	tropical
+1	JJ	indigenous
+1	JJ	temperate
+1	JJ	hot
+1	JJ	lucky
+1	JJ	simple
+1	JJ	tiny
+1	JJ	hard	
+1	JJ	sensational
+1	JJ	comparable
+1	JJ	angolian
+1	JJ	y
