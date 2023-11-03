ba4cc4951e8ac1efb1730cf87faaad0dc744a7d0
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-03 20:54:17 +0000

    remove version file

--- pa6-cky-v3/java/run.bat
+++ pa6-cky-v3/java/run.bat
@@ -1,4 +1,10 @@
 
 REM You may find this shell script helpful.
 
-SET DAT
+SET DATA=miniTest
+REM SET DATA=masc
+
+
+REM --parser nlpclass.assignments.PCFGParserTester$PCFGParser \
+
+REM java -server -mx500m -cp classes nlpclass.assignments.PCFGParserTester --parser nl
