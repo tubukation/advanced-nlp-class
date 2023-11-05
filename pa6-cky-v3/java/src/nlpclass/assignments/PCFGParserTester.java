52433bdd1a474e7284971e6395fd27de67609076
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-05 12:03:40 +0000

    cpufreq: prevents NULL pointer reference while all_time_in_state stats is collected.

--- pa6-cky-v3/java/src/nlpclass/assignments/PCFGParserTester.java
+++ pa6-cky-v3/java/src/nlpclass/assignments/PCFGParserTester.java
@@ -330,3 +330,189 @@ public class PCFGParserTester {
             Tree<String> result = makeTree(backs, 0, n, bestKey);
             if (!"ROOT".equals(result.getLabel())) {
                 List<Tree<String>> children = new ArrayList<Tree<String>>();
+                children.add(result);
+                result = new Tree<String>("ROOT", children); // !@#$
+            }
+            
+            /*
+            System.out.println("==================================================");
+            System.out.println(result);
+            System.out.println("====================^^^^^^========================");
+            */
+            return TreeAnnotations.unAnnotateTree(result);
+        }
+        
+    }
+
+  // BaselineParser =============================================================
+
+  /**
+   * Baseline parser (though not a baseline I've ever seen before).  Tags the
+   * sentence using the baseline tagging method, then either retrieves a known
+   * parse of that tag sequence, or builds a right-branching parse for unknown
+   * tag sequences.
+   */
+    public static class BaselineParser implements Parser {
+
+        CounterMap<List<String>,Tree<String>> knownParses;
+        CounterMap<Integer,String> spanToCategories;
+        Lexicon lexicon;
+
+        public void train(List<Tree<String>> trainTrees) {
+            lexicon = new Lexicon(trainTrees);
+            knownParses = new CounterMap<List<String>, Tree<String>>();
+            spanToCategories = new CounterMap<Integer, String>();
+            for (Tree<String> trainTree : trainTrees) {
+                List<String> tags = trainTree.getPreTerminalYield();
+                knownParses.incrementCount(tags, trainTree, 1.0);
+                tallySpans(trainTree, 0);
+            }
+        }
+
+    public Tree<String> getBestParse(List<String> sentence) {
+      List<String> tags = getBaselineTagging(sentence);
+      if (knownParses.keySet().contains(tags)) {
+        return getBestKnownParse(tags, sentence);
+      }
+      return buildRightBranchParse(sentence, tags);
+    }
+
+    /* Builds a tree that branches to the right.  For pre-terminals it
+     * uses the most common tag for the word in the training corpus.
+     * For all other non-terminals it uses the tag that is most common
+     * in training corpus of tree of the same size span as the tree
+     * that is being labeled. 
+     */
+    private Tree<String> buildRightBranchParse(List<String> words, List<String> tags) {
+      int currentPosition = words.size() - 1;
+      Tree<String> rightBranchTree = buildTagTree(words, tags, currentPosition);
+      while (currentPosition > 0) {
+        currentPosition--;
+        rightBranchTree = merge(buildTagTree(words, tags, currentPosition),
+                                rightBranchTree);
+      }
+      rightBranchTree = addRoot(rightBranchTree);
+      return rightBranchTree;
+    }
+
+    private Tree<String> merge(Tree<String> leftTree, Tree<String> rightTree) {
+        int span = leftTree.getYield().size() + rightTree.getYield().size();
+        String mostFrequentLabel = spanToCategories.getCounter(span).argMax();
+        List<Tree<String>> children = new ArrayList<Tree<String>>();
+        children.add(leftTree);
+        children.add(rightTree);
+        return new Tree<String>(mostFrequentLabel, children);
+    }
+
+    private Tree<String> addRoot(Tree<String> tree) {
+      return new Tree<String>("ROOT", Collections.singletonList(tree));
+    }
+
+    private Tree<String> buildTagTree(List<String> words,
+                                      List<String> tags,
+                                      int currentPosition) {
+      Tree<String> leafTree = new Tree<String>(words.get(currentPosition));
+      Tree<String> tagTree = new Tree<String>(tags.get(currentPosition), 
+                                              Collections.singletonList(leafTree));
+      return tagTree;
+    }
+    
+    private Tree<String> getBestKnownParse(List<String> tags, List<String> sentence) {
+        Tree<String> parse = knownParses.getCounter(tags).argMax().deepCopy();
+        parse.setWords(sentence);
+        return parse;
+    }
+
+    private List<String> getBaselineTagging(List<String> sentence) {
+      List<String> tags = new ArrayList<String>();
+      for (String word : sentence) {
+        String tag = getBestTag(word);
+        tags.add(tag);
+      }
+      return tags;
+    }
+
+    private String getBestTag(String word) {
+      double bestScore = Double.NEGATIVE_INFINITY;
+      String bestTag = null;
+      for (String tag : lexicon.getAllTags()) {
+        double score = lexicon.scoreTagging(word, tag);
+        if (bestTag == null || score > bestScore) {
+          bestScore = score;
+          bestTag = tag;
+        }
+      }
+      return bestTag;
+    }
+
+    private int tallySpans(Tree<String> tree, int start) {
+        if (tree.isLeaf() || tree.isPreTerminal()) {
+            return 1;
+        }
+        int end = start;
+        for (Tree<String> child : tree.getChildren()) {
+            int childSpan = tallySpans(child, end);
+            end += childSpan;
+        }
+        String category = tree.getLabel();
+        if (! category.equals("ROOT")) {
+            spanToCategories.incrementCount(end - start, category, 1.0);
+        }
+        return end - start;
+    }
+
+  }
+
+  // TreeAnnotations ============================================================
+
+  /**
+   * Class which contains code for annotating and binarizing trees for
+   * the parser's use, and debinarizing and unannotating them for
+   * scoring.
+   */
+    public static class TreeAnnotations {
+
+        public static Tree<String> annotateTree(Tree<String> unAnnotatedTree) {
+
+            // Currently, the only annotation done is a lossless binarization
+
+            // TODO: change the annotation from a lossless binarization to a
+            // finite-order markov process (try at least 1st and 2nd order)
+            // mark nodes with the label of their parent nodes, giving a second
+            // order vertical markov process
+
+            return binarizeTree(unAnnotatedTree);
+        }
+    
+        private static Tree<String> binarizeTree(Tree<String> tree) {
+            String label = tree.getLabel();
+            if (tree.isLeaf()) {
+                return new Tree<String>(label);
+            }
+            
+            /*
+            // [Average]    P: 48.53   R: 47.67   F1: 48.10   EX:  4.85
+            if (tree.getChildren().size() == 1) {
+                return new Tree<String>(label, Collections.singletonList(binarizeTree(tree.getChildren().get(0))));
+            }
+          */
+          
+          //  [Average]    P: 48.53   R: 47.67   F1: 48.10   EX:  4.85              
+            if (tree.getChildren().size() <= 2) {
+                List<Tree<String>> children = new ArrayList<Tree<String>>(2);
+                for (Tree<String> child: tree.getChildren()) { 
+                    children.add(binarizeTree(child));
+                }
+                return new Tree<String>(label, children);
+            }
+       
+            // otherwise, it's a binary-or-more local tree, 
+            // so decompose it into a sequence of binary and unary trees.
+            String intermediateLabel = "@" + label + "->";
+            Tree<String> intermediateTree = binarizeTreeHelper(tree, 0, intermediateLabel);
+            return new Tree<String>(label, intermediateTree.getChildren());
+        }
+
+        private static Tree<String> binarizeTreeHelper(Tree<String> tree,
+                                                   int numChildrenGenerated, 
+                                                   String intermediateLabel) {
