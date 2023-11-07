c6dd54aafda90afd06b20ff0e70689470b5692a5
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-07 05:25:23 +0000

    Update customCommands.js

--- pa6-cky-v3/java/src/nlpclass/assignments/Submit.java
+++ pa6-cky-v3/java/src/nlpclass/assignments/Submit.java
@@ -263,3 +263,186 @@ public class Submit {
     List<String> partNames = validParts();
     return (partId >= 1 && partId <= partNames.size() + 1);
   }
+
+  private int promptPart() {
+    int partId = -1;
+    System.out.println("== Select which part(s) to submit:");
+    List<String> partNames = validParts();
+    List<List<String>> srcFiles = sources();
+    StringBuffer prompt = new StringBuffer();
+    for(int i = 1; i < partNames.size() + 1; i++) {
+      prompt.append(String.format("==  %d) %s [", i, partNames.get(i - 1)));
+      List<String> srcs = srcFiles.get(i - 1);
+      for(String src : srcs) {
+        prompt.append(String.format(" %s ", src));
+      }
+      prompt.append("]\n"); 
+    }
+    prompt.append(String.format("==  %d) All of the above \n", partNames.size() + 1));
+    prompt.append(String.format("==\nEnter your choice [1-%d]: ", partNames.size() + 1));
+    System.out.println(prompt.toString());
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      String line = in.readLine();
+      partId = Integer.parseInt(line);
+      if(!isValidPartId(partId)) {
+        partId = -1;
+      }
+    } catch (Exception e) {
+      System.err.println("!! Error reading partId from stdin: " + e.getMessage());
+      return -1;
+    }
+    return partId;
+  }
+
+
+  // Returns [email,ch,signature]
+  private String[] getChallenge(String email, int partId) {
+      System.out.println("\n== getting challenge ...");
+    String [] results = new String[4];
+    try {
+      URL url = new URL(challenge_url());
+      System.out.println("== about to open connection");
+      URLConnection connection = url.openConnection();
+      System.out.println("== opened connection");
+      connection.setDoOutput(true);
+      OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+      // url encode e-mail
+      out.write("email_address=" + URLEncoder.encode(email, "UTF-8"));
+      out.write("&assignment_part_sid=" + String.format("%s-%s", homework_id(), partId));
+      out.write("&response_encoding=delim");
+      out.close();
+      BufferedReader in = new BufferedReader(
+                                new InputStreamReader(connection.getInputStream()));
+      StringBuffer sb = new StringBuffer();
+      String line;
+      while((line = in.readLine()) != null) {
+        sb.append(line + "\n");
+      }
+      String str = sb.toString(); 
+      in.close();
+
+      String[] splits = str.split("\\|"); 
+
+      if(splits.length < 8) {
+        System.err.println("!! Error getting challenge from server.");
+        for(String string : results) {
+          System.err.println(string);
+        }
+        return null;
+      } else {
+        results[0] = splits[2]; // email
+        results[1] = splits[4]; // ch
+        results[2] = splits[6]; // signature
+        if(splits.length == 9) { // if there's a challenge, use it
+          results[3] = splits[8];
+        } else {
+          results[3] = null;
+        }
+      }
+    } catch (Exception e) {
+      System.err.println("Error getting challenge from server: " + e.getMessage());
+    }
+    System.out.println("== got challenge");
+    return results;
+  }
+
+  private String submitSolution(String email, String ch_resp, int part, String output,
+                                  String source, String state) {
+    String str = null;
+    try {
+      StringBuffer post = new StringBuffer();
+      post.append("assignment_part_sid=" + URLEncoder.encode(
+          String.format("%s-%d", homework_id(), part), "UTF-8"));
+      post.append("&email_address=" + URLEncoder.encode(email, "UTF-8"));
+      post.append("&submission=" + URLEncoder.encode(base64encode(output), "UTF-8"));
+      post.append("&submission_aux=" + URLEncoder.encode(base64encode(source), "UTF-8"));
+      post.append("&challenge_response=" + URLEncoder.encode(ch_resp, "UTF-8"));
+      post.append("&state=" + URLEncoder.encode(state, "UTF-8"));
+
+      URL url = new URL(submit_url());
+      URLConnection connection = url.openConnection();
+      connection.setDoOutput(true);
+      OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+      out.write(post.toString());
+      out.close();
+
+      BufferedReader in = new BufferedReader(
+                                new InputStreamReader(connection.getInputStream()));
+      str = in.readLine();
+      in.close();
+      
+    } catch (Exception e) {
+      System.err.println("!! Error submitting solution: " + e.getMessage());
+      return null;
+    }
+    return str;
+  }
+
+
+  // =========================== LOGIN HELPERS ===========================
+
+  // Returns [login, password]
+  private String[] loginPrompt() {
+    String[] results = new String[2];
+    try {
+      //System.out.print("Login (Email address): ");
+      System.out.println("Login (Email address): ");
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      String line = in.readLine();
+      results[0] = line.trim();
+
+      //System.out.print("Password: ");
+      System.out.println("Password: ");
+      line = in.readLine();
+      results[1] = line.trim();
+    } catch (IOException e) {
+      System.err.println("!! Error prompting for login/password: " + e.getMessage());
+    }
+    return results;
+  }
+
+  private String challengeResponse(String email, String passwd, String challenge) {
+    MessageDigest md = null;
+    try {
+      md = MessageDigest.getInstance("SHA-1");
+    } catch (NoSuchAlgorithmException e) {
+      System.err.println("No such hashing algorithm: " + e.getMessage());
+    }
+    try {
+      String message = challenge + passwd;
+      md.update(message.getBytes("US-ASCII"));
+      byte[] byteDigest = md.digest();
+      StringBuffer buf = new StringBuffer();
+      for(byte b : byteDigest) {
+        buf.append(String.format("%02x",b));
+      }
+      return buf.toString();
+    } catch (Exception e) {
+      System.err.println("Error generating challenge response: " + e.getMessage());
+    }
+    return null;
+  }
+
+  public String base64encode(String str) {
+    Base64 base = new Base64();
+    byte[] strBytes = str.getBytes();
+    byte[] encBytes = base.encode(strBytes);
+    String encoded = new String(encBytes);
+    return encoded;
+  }
+
+
+  public static void main(String [] args) {
+    /*
+    if (args.length < 2 || args[0].equals("-h")) {
+      System.err.println("usage:\tjava -jar pcfg.jar submit grammar_file1 [grammar_file2 [...]]");
+      System.exit(0);
+    }
+
+    grammarFiles = args;
+    */
+    Submit submit = new Submit();
+    submit.submit(0);
+  }
+}
