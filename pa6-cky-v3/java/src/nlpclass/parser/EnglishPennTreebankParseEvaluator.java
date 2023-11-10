4ebcaac0d762f6c44276d12e7f61a1e69b80b5dc
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-10 14:47:02 +0000

    Remove imports

--- pa6-cky-v3/java/src/nlpclass/parser/EnglishPennTreebankParseEvaluator.java
+++ pa6-cky-v3/java/src/nlpclass/parser/EnglishPennTreebankParseEvaluator.java
@@ -1,1 +1,11 @@
-package nlpclass.p
+package nlpclass.parser;
+
+import nlpclass.ling.Tree;
+import nlpclass.ling.Trees;
+
+import java.util.*;
+import java.io.PrintWriter;
+import java.io.StringReader;
+
+/**
+ * Evaluates precision and recall for English Penn Treebank parse
