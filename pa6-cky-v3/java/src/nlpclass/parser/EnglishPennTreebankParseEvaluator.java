090f5bc8f64d805422ae5c3cf34d40ad7aaa14bc
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-11 05:42:24 +0000

    mac80211: fix queues stuck issue with HT bandwidth change

--- pa6-cky-v3/java/src/nlpclass/parser/EnglishPennTreebankParseEvaluator.java
+++ pa6-cky-v3/java/src/nlpclass/parser/EnglishPennTreebankParseEvaluator.java
@@ -9,3 +9,7 @@ import java.io.StringReader;
 
 /**
  * Evaluates precision and recall for English Penn Treebank parse
+ * trees.  NOTE: Unlike the standard evaluation, multiplicity over
+ * each span is ignored.  Also, punctuation is NOT currently deleted
+ * properly (approximate hack), and other normalizations (like AVDP ~
+ * 
