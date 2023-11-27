74aa25bfb960e9712a8a94011ec40e60246d9257
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-27 11:47:49 +0000

    Translated by blues

--- pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
+++ pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
@@ -4,4 +4,12 @@ import java.util.Map;
 import java.util.HashMap;
 
 /**
- * Utilities for managing command line flags 
+ * Utilities for managing command line flags and arguments.
+ *
+ * @author Dan Klein
+ */
+public class CommandLineUtils {
+
+  /**
+   * Simple method which turns an array of command line arguments into a
+   * map, where each token sta
