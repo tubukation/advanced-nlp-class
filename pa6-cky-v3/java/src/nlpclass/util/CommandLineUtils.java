d87c000c1d86f7f8c39346d0ebaa11ea1242181b
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-27 16:06:17 +0000

    Rename test files

--- pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
+++ pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
@@ -12,4 +12,7 @@ public class CommandLineUtils {
 
   /**
    * Simple method which turns an array of command line arguments into a
-   * map, where each token sta
+   * map, where each token starting with a '-' is a key and the following
+   * non '-' initial token, if there is one, is the value.  For example,
+   * '-size 5 -verbose' will produce a map with entries (-size, 5) and
+ 
