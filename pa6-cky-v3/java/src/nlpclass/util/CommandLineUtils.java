df6369a057aa0d92b3f6c035e9d4e0f5e5f73b48
tubukation <166604354+tubukation@users.noreply.github.com>
2023-11-28 12:26:06 +0000

    add keys

--- pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
+++ pa6-cky-v3/java/src/nlpclass/util/CommandLineUtils.java
@@ -15,4 +15,8 @@ public class CommandLineUtils {
    * map, where each token starting with a '-' is a key and the following
    * non '-' initial token, if there is one, is the value.  For example,
    * '-size 5 -verbose' will produce a map with entries (-size, 5) and
- 
+   * (-verbose, null).
+   */
+    public static Map<String, String> simpleCommandLineParser(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+        
