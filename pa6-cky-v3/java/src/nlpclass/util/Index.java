83a35025f4065427e6db9913cdc1ad53110f0d4e
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-01 20:19:59 +0000

    update nl80211.h

--- pa6-cky-v3/java/src/nlpclass/util/Index.java
+++ pa6-cky-v3/java/src/nlpclass/util/Index.java
@@ -11,3 +11,66 @@ import java.util.*;
  * @author Dan Klein
  */
 public class Index <E> extends AbstractList<E> {
+  List<E> objects;
+  Map<E, Integer> indexes;
+
+  /**
+   * Return the object with the given index
+   *
+   * @param index
+   */
+  public E get(int index) {
+    return objects.get(index);
+  }
+
+  /**
+   * Returns the number of objects indexed.
+   */
+  public int size() {
+    return objects.size();
+  }
+
+  /**
+   * Returns the index of the given object, or -1 if the object is not present
+   * in the indexer.
+   *
+   * @param o
+   */
+  public int indexOf(Object o) {
+    Integer index = indexes.get(o);
+    if (index == null)
+      return -1;
+    return index;
+  }
+
+  /**
+   * Constant time override for contains.
+   */
+  public boolean contains(Object o) {
+    return indexes.keySet().contains(o);
+  }
+
+  /**
+   * Add an element to the indexer.  If the element is already in the indexer,
+   * the indexer is unchanged (and false is returned).
+   *
+   * @param e
+   */
+  public boolean add(E e) {
+    if (contains(e)) return false;
+    objects.add(e);
+    indexes.put(e, size() - 1);
+    return true;
+  }
+
+  public Index() {
+    objects = new ArrayList<E>();
+    indexes = new HashMap<E, Integer>();
+  }
+
+  public Index(Collection<? extends E> c) {
+    this();
+    addAll(c);
+  }
+
+}
---
+++ pa6-cky-v3/java/src/nlpclass/util/Interner.java
@@ -0,0 +1,7 @@
+package nlpclass.util;
+
+import java.util.Map;
+
+/**
+ * Canonicalizes objects.  Given an object, the intern() method returns a
+ * c
