ed1ef4d1eda5a5875f341c70e19acf96ab2e65c4
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-02 18:42:14 +0000

    corrected paths

--- pa6-cky-v3/java/src/nlpclass/util/Interner.java
+++ pa6-cky-v3/java/src/nlpclass/util/Interner.java
@@ -6,4 +6,6 @@ import java.util.Map;
  * Canonicalizes objects.  Given an object, the intern() method returns a
  * canonical representation of that object, that is, an object which equals()
  * the input.  Furthermore, given two objects x and y, it is guaranteed that if
- * x.equals(y), then intern(x) == in
+ * x.equals(y), then intern(x) == intern(y).  The default behavior is that the
+ * interner is backed by a HashMap and the canonical version of an object x is
+ * simply the first object that equals(x) which is passed to the
