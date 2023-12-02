dc7e47c0d89fd0e071af4750774e569c9f98f9b3
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-02 07:39:50 +0000

    l10n: Updates to Croatian (hr) translation

--- pa6-cky-v3/java/src/nlpclass/util/Interner.java
+++ pa6-cky-v3/java/src/nlpclass/util/Interner.java
@@ -4,4 +4,6 @@ import java.util.Map;
 
 /**
  * Canonicalizes objects.  Given an object, the intern() method returns a
- * c
+ * canonical representation of that object, that is, an object which equals()
+ * the input.  Furthermore, given two objects x and y, it is guaranteed that if
+ * x.equals(y), then intern(x) == in
