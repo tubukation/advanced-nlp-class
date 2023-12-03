712615c72603c5980a4d1cf3205e09006d041e0d
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-03 05:41:22 +0000

    Update tnbot.xml

--- pa6-cky-v3/java/src/nlpclass/util/Interner.java
+++ pa6-cky-v3/java/src/nlpclass/util/Interner.java
@@ -8,4 +8,7 @@ import java.util.Map;
  * the input.  Furthermore, given two objects x and y, it is guaranteed that if
  * x.equals(y), then intern(x) == intern(y).  The default behavior is that the
  * interner is backed by a HashMap and the canonical version of an object x is
- * simply the first object that equals(x) which is passed to the
+ * simply the first object that equals(x) which is passed to the interner.  In
+ * this case, it can be true that intern(x) == x.  The backing map can be
+ * specified by passing a MapFactory on construction (though the only standard
+ * option wh
