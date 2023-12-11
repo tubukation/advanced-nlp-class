3f5be411ed394dfb766273d9cb12e3a5d3eeea53
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-11 05:38:37 +0000

    Init files

--- pa6-cky-v3/java/src/nlpclass/util/MapFactory.java
+++ pa6-cky-v3/java/src/nlpclass/util/MapFactory.java
@@ -6,4 +6,12 @@ import java.io.Serializable;
 /**
  * The MapFactory is a mechanism for specifying what kind of map is to be used
  * by some object.  For example, if you want a Counter which is backed by an
- * IdentityHashMap instead of the defaul Ha
+ * IdentityHashMap instead of the defaul HashMap, you can pass in an
+ * IdentityHashMapFactory.
+ *
+ * @author Dan Klein
+ */
+
+public abstract class MapFactory<K,V> implements Serializable {
+
+  public static class HashMapFactory<
