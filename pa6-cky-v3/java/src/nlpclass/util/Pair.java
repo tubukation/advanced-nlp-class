0be07a797fe3f1a76c73415c1d35f01b74f8b7eb
tubukation <166604354+tubukation@users.noreply.github.com>
2023-12-13 10:39:18 +0000

    express

--- pa6-cky-v3/java/src/nlpclass/util/Pair.java
+++ pa6-cky-v3/java/src/nlpclass/util/Pair.java
@@ -4,4 +4,17 @@ package nlpclass.util;
  * A generic-typed pair of objects.
  * @author Dan Klein
  */
-public class P
+public class Pair<F,S> {
+  F first;
+  S second;
+
+  public F getFirst() {
+    return first;
+  }
+
+  public S getSecond() {
+    return second;
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) ret
