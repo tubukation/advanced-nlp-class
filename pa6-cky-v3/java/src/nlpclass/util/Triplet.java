266aad3a2c3e12a31904e624cc01fa114b15e53b
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-03 16:05:04 +0000

    updat

--- pa6-cky-v3/java/src/nlpclass/util/Triplet.java
+++ pa6-cky-v3/java/src/nlpclass/util/Triplet.java
@@ -6,4 +6,19 @@ package nlpclass.util;
  */
 public class Triplet<F,S,T> {
     F first;
-    S secon
+    S second;
+    T third;
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public T getThird() {
+        return third;
+    }
+
+    public b
