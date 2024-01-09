d5bfd77badfe438493bfb2cd89ada151c16e291c
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-09 06:45:33 +0000

    VC7 catching up

--- pa6-cky-v3/java/src/nlpclass/util/UnorderedPair.java
+++ pa6-cky-v3/java/src/nlpclass/util/UnorderedPair.java
@@ -1,1 +1,11 @@
-package n
+package nlpclass.util;
+
+/**
+ * A generic-typed unordered pair of objects.
+ * @author Dan Klein
+ */
+public class UnorderedPair<F,S> {
+  F first;
+  S second;
+
+  public F getFirs
