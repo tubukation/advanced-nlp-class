004d387e5098d338039e5dc475246c5de0a1e722
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-14 20:37:30 +0000

    Enable all tests

--- pa6-cky-v3/python/io/MASCTreebankReader.py
+++ pa6-cky-v3/python/io/MASCTreebankReader.py
@@ -1,1 +1,13 @@
 import os
+import sys
+
+from io.NumberRangeFileFilter import NumberRangeFileFilter
+import ling.Trees as Trees
+
+
+class TreeCollection:
+    """
+    Collection of Trees.
+    """
+
+    def _
