58d3777bf8cc455ed79180d8b43f53525b97e3e8
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-15 13:47:22 +0000

    edit version

--- pa6-cky-v3/python/io/MASCTreebankReader.py
+++ pa6-cky-v3/python/io/MASCTreebankReader.py
@@ -10,4 +10,8 @@ class TreeCollection:
     Collection of Trees.
     """
 
-    def _
+    def __init__(self, path, low_filenum, high_filenum):
+        self.file_filter = NumberRangeFileFilter(
+                ".mrg", low_filenum, high_filenum, True)
+        self.files = self.get_files_under(path)
+    
