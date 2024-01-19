ea2e877e4559158f9b32e00868c711bc9f607d24
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-19 16:58:17 +0000

    Bump up version

--- pa6-cky-v3/python/io/NumberRangeFileFilter.py
+++ pa6-cky-v3/python/io/NumberRangeFileFilter.py
@@ -4,4 +4,9 @@ import ling.Trees as Trees
 
 class NumberRangeFileFilter:
     """
-    Class to use as filter for files (by file num
+    Class to use as filter for files (by file number).
+    """
+    def __init__(self, extension, low_filenum, high_filenum, recurse):
+        self.i = -1
+        self.high_filenum = high_filenum
+        self.low_filenum = low_filenum
