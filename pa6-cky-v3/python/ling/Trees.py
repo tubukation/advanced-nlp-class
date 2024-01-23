1ab827eb6e2502fb23680450315b5d7a78f29875
tubukation <166604354+tubukation@users.noreply.github.com>
2024-01-23 13:45:03 +0000

    rdoc

--- pa6-cky-v3/python/ling/Trees.py
+++ pa6-cky-v3/python/ling/Trees.py
@@ -1,3 +1,12 @@
 from Tree import Tree
 
-# TODO: should I replace rendering of tre
+# TODO: should I replace rendering of tree.label to str(tree.label)??
+
+##################
+# Class Methods
+##################
+ROOT_LABEL = "ROOT"
+
+class TreeTransformer:
+    """
+        Abstract base class for different Tree transf
