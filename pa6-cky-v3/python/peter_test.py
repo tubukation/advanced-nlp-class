156cd4d7f6f2c81c9e9570b43d8d7c71799d12b6
tubukation <166604354+tubukation@users.noreply.github.com>
2024-02-25 12:15:04 +0000

    incremented version & release

--- pa6-cky-v3/python/peter_test.py
+++ pa6-cky-v3/python/peter_test.py
@@ -8,3 +8,10 @@ import ling.Trees as Trees
 import pennParser.EnglishPennTreebankParseEvaluator as EnglishPennTreebankParseEvaluator
 import io.PennTreebankReader as PennTreebankReader
 import io.MASCTreebankReader as MASCTreebankReader
+
+from PCFGParserTester import *
+
+if __name__ == '__main__':
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option('--path', dest='path', default='../data/parser')
+    opt_parser.add_
