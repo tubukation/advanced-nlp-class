b63894efecad0e6afc51d2f244ba7b44365a39af
tubukation <166604354+tubukation@users.noreply.github.com>
2024-02-26 14:00:11 +0000

    fat: fix possible overflow for fat_clusters

--- pa6-cky-v3/python/peter_test.py
+++ pa6-cky-v3/python/peter_test.py
@@ -14,4 +14,10 @@ from PCFGParserTester import *
 if __name__ == '__main__':
     opt_parser = optparse.OptionParser()
     opt_parser.add_option('--path', dest='path', default='../data/parser')
-    opt_parser.add_
+    opt_parser.add_option('--maxLength', dest='max_length', default='20')
+    opt_parser.add_option('--testData', dest='test_data', default='')
+
+    options, args = opt_parser.parse_args()
+    options = vars(options)
+
+    print '
