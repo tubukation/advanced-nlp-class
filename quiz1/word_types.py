93b43f18b09b5ecf2335516ab8adbcb490801844
tubukation <166604354+tubukation@users.noreply.github.com>
2024-03-06 06:55:54 +0000

    update versioning

--- quiz1/word_types.py
+++ quiz1/word_types.py
@@ -3,3 +3,9 @@ import re
 _RE_TOKEN = re.compile('[^a-zA-Z]+')
 
 def tokenize(text):
+    "Tokenize text by splitting on non-alphabetic characters"
+    return [x for x in _RE_TOKEN.split(text) if x]
+      
+def get_variants(types): 
+    """types is a set of words
+   
