350526a0cdd3de7798a2bc7311db1f24e513d132
tubukation <166604354+tubukation@users.noreply.github.com>
2024-03-09 08:53:53 +0000

    WORK IN PROGRESS

--- quiz2/bigram.py
+++ quiz2/bigram.py
@@ -4,4 +4,9 @@ import re
 
 _RE_TOKEN = re.compile(r'\s')
 
-def tokenize(
+def tokenize(text):
+    "Tokenize text by splitting on spaces"
+    return [x for x in _RE_TOKEN.split(text) if x]
+
+def get_lines(text):
+    lines = [ln.strip() for ln in text.split('
